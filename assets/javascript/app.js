99f41202186f3083c09d005ed17711bf9b4e19e9
thejunglife <thejunglife@gmail.com>
2019-09-18 21:20:07 -0700

    added some html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,48 +3,61 @@
 
 const myQuestions = [
   {
-      question: 'What NBA team has the most Championships?'
-
-      answers: {
-          a: 'Los Angeles Lakers'
-          b: 'Chicago Bulls'
-          c: 'Boston Celtics'
-          d: 'Golden State Warriors'
-      }
-      correctAnswer: 'c'
-  },
-  {
-    question: 'Which player has won the most championships?'
+    question: 'What NBA team has the most Championships?',
 
     answers: {
-        a: 'Kobe Bryant'
-        b: 'Micheal Jordan'
-        c: 'Robert Horry'
-        d: 'Bill Russell'
-    }
+      a: 'Los Angeles Lakers',
+      b: 'Chicago Bulls',
+      c: 'Boston Celtics',
+      d: 'Golden State Warriors'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Which player has won the most championships?',
+
+    answers: {
+      a: 'Kobe Bryant',
+      b: 'Micheal Jordan',
+      c: 'Robert Horry',
+      d: 'Bill Russell'
+    },
     correctAnswer: 'd'
   },
   {
-      question: 'Which NBA player has coined the phrase "Taco Tuesday" recently?'
+    question: 'Which NBA player has coined the phrase "Taco Tuesday" recently?',
 
-      answers: {
-          a: 'Lebron James'
-          b: 'Steph Curry'
-          c: 'Kevin Durant'
-          d: 'James Harden'
-      }
-      correctAnswer: 'a'
+    answers: {
+      a: 'Lebron James',
+      b: 'Steph Curry',
+      c: 'Kevin Durant',
+      d: 'James Harden'
+    },
+    correctAnswer: 'a'
   },
   {
-      question: 'How many 3 pointers has Ben Simmons made from 2017-2019?'
+    question: 'How many 3 pointers has Ben Simmons made from 2017-2019?',
 
-      answers: {
-          a: 100
-          b: 0
-          c: 87
-          d: 3
-      }
-      correctAnswer: 'b'
+    answers: {
+      a: 100,
+      b: 0,
+      c: 87,
+      d: 3
+    },
+    correctAnswer: 'b'
   }
 
 ]
+
+let correct = 0;
+let wrong = 0;
+let question = '';
+let answer = '';
+let userGuess = '';
+
+
+// need a start button to start the quiz
+
+document.getElementById('startGame').addEventListener('click', function () {
+    console.log(ping)
+})
